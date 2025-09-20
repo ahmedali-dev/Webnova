7790731927f6f3d6d6c03e1ea36ef7545bceec0d
ahmedali-dev <mohamedali850100@gmail.com>
2025-09-20 17:01:38 +0300

    update start file

--- start.php
+++ start.php
@@ -6,6 +6,8 @@ if (strtoupper(substr($os, 0, 3)) === 'WIN') {
     shell_exec("php -S localhost:8080 -t public .\public\index.php");
 } elseif (strtoupper($os) === 'DARWIN') {
     echo "Mac";
+    shell_exec("php -S localhost:8080 -t public public/index.php");
 } else {
     echo "Linux/Unix";
+    shell_exec("php -S localhost:8080 -t public public/index.php");
 }
